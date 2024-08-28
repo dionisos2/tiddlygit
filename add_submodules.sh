dc79db671c055167daa9c0c63ada2ca069b21a01
dionisos <denis.baudouin@gmail.com>
2024-08-28 13:53:00 +0200

    "Not adding submodules again"

---
+++ add_submodules.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+git submodule add https://github.com/OokTech/TW5-Bob.git plugins_repositories/TW5-Bob
+git submodule add https://github.com/kookma/TW-Shiraz.git plugins_repositories/TW-Shiraz
+git submodule add https://github.com/flibbles/tw5-relink.git plugins_repositories/tw5-relink
+git submodule add https://github.com/kookma/TW-Trashbin plugins_repositories/TW-Trashbin
+git submodule add https://github.com/kookma/TW-Commander.git plugins_repositories/TW-Commander
+git submodule add https://github.com/rimi/tw5-telmiger-plugins plugins_repositories/tw5-telmiger-plugins
--- installation.sh
+++ installation.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-git submodule add https://github.com/OokTech/TW5-Bob.git plugins_repositories/TW5-Bob
-git submodule add https://github.com/kookma/TW-Shiraz.git plugins_repositories/TW-Shiraz
-git submodule add https://github.com/flibbles/tw5-relink.git plugins_repositories/tw5-relink
-git submodule add https://github.com/kookma/TW-Trashbin plugins_repositories/TW-Trashbin
-git submodule add https://github.com/kookma/TW-Commander.git plugins_repositories/TW-Commander
-git submodule add https://github.com/rimi/tw5-telmiger-plugins plugins_repositories/tw5-telmiger-plugins
-
 git config merge.tiddly-merge.name "merge for TiddlyWiki tid files"
 git config merge.tiddly-merge.driver "tiddly-merge.py %O %A %B"
 
